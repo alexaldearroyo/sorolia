78c6bbd2944d78f53a539cf6facddacdff7fecb0
Alex Aldearroyo <alexaldearroyo@gmail.com>
2026-04-12 21:30:10 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: process.env.BASE_PATH || '/',
   plugins: [tailwindcss(), svelte()]
 });
